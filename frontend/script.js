842d14f77348da6cf815aa11985b1187946c65d6
neshanthkishor <neshanthkishor.s@gmail.com>
2026-08-14 09:18:45 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -13,7 +13,7 @@ async function analyzeRepository() {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:5000/analyze", {
+        const response = await fetch("https://github-health-analyzer.onrender.com/analyze", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
